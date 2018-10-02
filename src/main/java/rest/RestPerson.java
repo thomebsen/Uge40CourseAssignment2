8d1cf49b559968320912f7be4e1caed297dd853f
Srax <thomas_ebsen@live.dk>
2018-10-02 12:41:08 +0200

    renamed packages

--- src/main/java/rest/RestPerson.java
+++ src/main/java/rest/RestPerson.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.uge40courseassignment2;
+package rest;
 
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;

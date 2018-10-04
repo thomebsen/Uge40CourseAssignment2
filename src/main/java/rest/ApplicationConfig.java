e82536250893dc9280fcf10cfde29a0e1f7cf2f4
Srax <thomas_ebsen@live.dk>
2018-10-04 11:21:48 +0200

    Trying to delete errormessage folder

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -29,8 +29,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(errorhandling.ParamaterNoMatchExceptionMapper.class);
-        resources.add(errorhandling.PersonNotFoundExceptionMapper.class);
         resources.add(rest.RestPerson.class);
     }
     
